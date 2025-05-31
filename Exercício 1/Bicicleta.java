c7ceec1c27f64123da5d3214720d2de6ab63cadf
Ryan-Sous <ryanmartinsdesousa10@gmail.com>
2025-05-31 14:43:39 -0400

    Exercício 1 - Commit 2: Adicionar métodos acelerar e frear na classe Bicicleta

--- Exercício 1/Bicicleta.java
+++ Exercício 1/Bicicleta.java
@@ -1,11 +1,29 @@
-// Bicicleta.java
 public class Bicicleta {
     private int velocidade;
     private int marcha;
 
-    // Construtor
     public Bicicleta() {
         this.velocidade = 0;
         this.marcha = 0;
     }
+
+    public void acelerar() {
+        velocidade += 5;
+        marcha++;
+    }
+
+    public void frear() {
+        if (velocidade >= 5) {
+            velocidade -= 5;
+        } else {
+            velocidade = 0;
+        }
+
+        if (marcha > 0) {
+            marcha--;
+        } else {
+            marcha = 0;
+        }
+    }
 }
+
