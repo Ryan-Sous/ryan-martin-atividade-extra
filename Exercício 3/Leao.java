472fe140c49604b673f38ff9f7b598aef1a1dd34
Ryan-Sous <ryanmartinsdesousa10@gmail.com>
2025-06-01 13:10:00 -0400

    Exercício 3 - Commit 3: Cria classe Leao que herda de Animal e implementa métodos emitirSom() e alimentar()

---
+++ Exercício 3/Leao.java
@@ -0,0 +1,11 @@
+public class Leao extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("O leão ruge!");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("O leão está se alimentando de carne.");
+    }
+}
